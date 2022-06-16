85b5d1f19641044367ffdbf18c5c7c81204e0185
alberto romano <aromanog1@gmail.com>
2022-06-15 23:48:31 -0500

    Finished sign in page design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
   has_many :inventories
   has_many :material_requests, class_name: 'MaterialRequest', foreign_key: 'who_requested_id'
   has_many :material_requests, class_name: 'MaterialRequest', foreign_key: 'who_authorized_id'
